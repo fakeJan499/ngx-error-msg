03a90733d48def3e9c7ccbfd52d94569d7c69e69
Dawid Szemro <63339381+fakeJan499@users.noreply.github.com>
2024-10-25 16:22:28 +0200

    feat: add mapper factory function (#22)
    Close #10

--- projects/example-standalone/src/app/translated-messages/translated-messages.component.ts
+++ projects/example-standalone/src/app/translated-messages/translated-messages.component.ts
@@ -1,22 +1,21 @@
-import { Component, inject, Injectable } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TranslateService } from '@ngx-translate/core';
-import { NgxErrorMsgDirective, NgxErrorMsgService, provideNgxErrorMsg } from 'ngx-error-msg';
+import { createNgxErrorMsgMapper, NgxErrorMsgDirective, provideNgxErrorMsg } from 'ngx-error-msg';
 
-@Injectable()
-class Mapper extends NgxErrorMsgService {
-    private readonly translateService = inject(TranslateService);
+const Mapper = createNgxErrorMsgMapper(() => {
+    const translateService = inject(TranslateService);
 
-    protected override errorMsgMappings = {
-        required: () => this.translateService.get('ERRORS.REQUIRED'),
+    return {
+        required: () => translateService.get('ERRORS.REQUIRED'),
         minlength: (error: any) =>
-            this.translateService.get('ERRORS.MIN_LENGTH', {
+            translateService.get('ERRORS.MIN_LENGTH', {
                 minLength: error.requiredLength,
             }),
     };
-}
+});
 
 @Component({
     selector: 'app-translated-messages',
